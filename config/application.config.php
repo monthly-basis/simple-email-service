835bec2de0b86e345e12241df1ad7d65ad05fdda
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-04 04:36:33 +0000

    Disable ads in views

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,7 @@ return [
         'Laminas\Db',
         'Laminas\Router',
         'MonthlyBasis\SimpleEmailService',
+        'MonthlyBasis\Website',
     ],
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
